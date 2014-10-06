8f881912810aa3088c3870c1ce74015c8f47e287
Jim Frode Hoff <jimfrodehoff@gmail.com>
2014-10-06 14:15:31 +0200

    should work in theory. Missing docs

--- classes/Smart_Mail_Reminder.php
+++ classes/Smart_Mail_Reminder.php
@@ -19,9 +19,10 @@ class Smart_Mail_Reminder {
 	}
 
 	private function __construct() {
-		add_action( 'admin_init' , array($this, 'admin_init'));
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'admin_init' , array($this, 'admin_init'));
 		add_action( 'init', array($this, 'register_acf_fields'));
+		add_action( 'save_post', array($this, 'reset_mail_sent_meta' ));
 	}
 
 	/* Initialization */
@@ -40,6 +41,9 @@ class Smart_Mail_Reminder {
 	}
 
 	public function admin_init() {
+
+		register_setting( 'reminder-settings-group', 'reminder_admin_receive_bool' );
+		register_setting( 'reminder-settings-group', 'reminder_admin_email' );
 	}
 
 	public function admin_menu() {
@@ -55,6 +59,7 @@ class Smart_Mail_Reminder {
 				/* @var $user WP_User */
 				$users[ $user->get("user_email") ] = $user->get("user_nicename");
 			}
+//			var_dump($users);
 			register_field_group(array (
 				'id' => 'acf_reminder-to-authors',
 				'title' => 'Reminder to author(s)',
@@ -83,29 +88,24 @@ class Smart_Mail_Reminder {
 					),
 					array (
 						'key' => 'field_542e88cd26298',
-						'label' => 'Mottaker(e)',
-						'name' => 'reminder_recipients',
-						'type' => 'checkbox',
+						'label' => 'Send til skribent',
+						'name' => 'reminder_author_bool',
+						'type' => 'true_false',
 						'required' => 1,
-						'choices' => array (
-							'author' => 'Skribent',
-							'editor' => 'Redaktør',
-							'admin' => 'Administrator',
-						),
-						'default_value' => '',
-						'layout' => 'vertical',
+						'message' => 'Huk av for å gi skribent påminnelse',
+						'default_value' => 0,
 					),
 					array (
 						'key' => 'field_54326012e38b1',
-						'label' => 'Flere mottakere',
-						'name' => 'reminder_extra_recipients',
+						'label' => 'Ekstra mottakere',
+						'name' => 'reminder_recipients',
 						'type' => 'repeater',
-						'instructions' => __('Legg til flere brukere som skal få påminnelse'),
-						'required' => 0,
+						'instructions' => 'Testing adding of users',
+						'required' => 1,
 						'sub_fields' => array (
 							array (
 								'key' => 'field_54326031e38b2',
-								'label' => 'Mottakere',
+								'label' => 'User',
 								'name' => 'user',
 								'type' => 'select',
 								'required' => 1,
@@ -116,7 +116,7 @@ class Smart_Mail_Reminder {
 								'multiple' => 0,
 							),
 						),
-						'row_min' => 0,
+						'row_min' => 1,
 						'row_limit' => '',
 						'layout' => 'table',
 						'button_label' => 'Legg til bruker',
@@ -147,30 +147,69 @@ class Smart_Mail_Reminder {
 
 	/* Views */
 	public function view_options() {
-		echo "<h1>It works</h1>";
-//		$users = array();
-//		foreach ( get_users() as $user ) {
-//			/* @var $user WP_User */
-//			$users[ $user->get("user_email") ] = $user->get("user_nicename");
-//		}
-//		var_dump($users);
+?>
+<div class="wrap">
+	<h2><?php _e('Smart Mail Reminder') ?></h2>
 
-		self::cron_daily();
-	}
+	<form method="post" action="options.php">
+		<?php settings_fields( 'reminder-settings-group' ); ?>
+		<?php do_settings_sections( 'reminder-settings-group' ); ?>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row"><?php _e('Administrators epost') ?></th>
+				<td><input type="text" name="reminder_admin_email" value="<?php echo esc_attr( get_option('reminder_admin_email') ); ?>" /></td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row"><label for="reminder_admin_receive_bool"><?php _e('Skal administrator få påminnelser?') ?></label></th>
+				<td>
+					<input type="checkbox" name="reminder_admin_receive_bool" id="reminder_admin_receive_bool" <?php
+					if(! esc_attr( get_option('reminder_admin_receive_bool') )) echo "un"; ?>checked />
+
+				</td>
+			</tr>
+		</table>
+
+		<?php submit_button(); ?>
+
+	</form>
+</div>
+
+<?php }
 
 	/* Controller */
+	public function reset_mail_sent_meta($post_id) {
+		self::set_mail_sent_meta($post_id, 0);
+	}
+
+	private static function set_mail_sent_meta($post_id, $value=0) {
+		if ( wp_is_post_revision( $post_id ) )
+			return;
+
+		if (get_post_meta($post_id, 'reminder_sent'))
+			update_post_meta($post_id, 'reminder_sent', $value, get_post_meta($post_id, 'reminder_sent'));
+		else
+			add_post_meta($post_id, 'reminder_sent', $value);
+	}
+
 	public static function cron_daily() {
 		$query_args = array(
 			'post_type'        => 'post',
 			'post_status'      => 'publish',
+			'meta_key'         => 'reminder_date',
+			'meta_value'       => date("Ymd"),
 			'suppress_filters' => true
-		);
+		); // Get all posts with raminderdate set to today
+
 		$posts = get_posts($query_args);
 		foreach ( $posts as $post ) {
 			/* @var $post WP_Post */
+			$meta = get_post_meta($post->ID);
+
+			if ($meta["reminder_sent"] && $meta["reminder_sent"] == "1")
+				continue; //filter all that have been sent today
 
 			$recipients = array();
-			$meta = get_post_meta($post->ID);
 			$reminder_date = $meta['reminder_date'][0];
 			if ($reminder_date && $reminder_date === date("Ymd")) {
 				$subject = "[" . get_option("blogname") . "] " . __("Automatisk varsel");
@@ -178,31 +217,34 @@ class Smart_Mail_Reminder {
 				$footer = __("Artikkel: ") . get_permalink($post->ID);
 
 
-				foreach ( get_post_meta( $post->ID, "reminder_recipients" )[0] as $recipient ) {
-					if ($recipient === "author") {
-						$recipients[] = get_the_author_meta('user_email', $post->post_author);
-					}
-					if ($recipient === "editor") {
-						//TODO: add editor recipient(s)
-					}
-					if ($recipient === "admin") {
-						//TODO: add admin recipient(s)
-					}
+				if ($meta["reminder_author_bool"][0] == "1") {
+					$recipients[] = get_the_author_meta('user_email', $post->post_author);
+				}
+				if (get_option("reminder_admin_receive_bool")) {
+					$recipients[] = get_option("reminder_admin_email");
 				}
 
-				for ($i = 0; $i < intval(get_post_meta($post->ID, "reminder_extra_recipients")[0], 10) ; $i++) {
-					$recipients[] = get_post_meta($post->ID, "reminder_extra_recipients_" . $i . "_user")[0];
+				for ($i = 0; $i < intval(get_post_meta($post->ID, "reminder_recipients")[0], 10) ; $i++) {
+					$recipients[] = get_post_meta($post->ID, "reminder_recipients_" . $i . "_user")[0];
 				}
-//				var_dump($recipients);
 
-				echo $subject;
-				echo $message;
-				echo $footer;
-
+//				var_dump($meta);
+				foreach ( self::remove_duplicates($recipients) as $recipient ) {
+					wp_mail($recipient, $subject . $footer, $message);
+				}
+				self::set_mail_sent_meta($post->ID, 1);
 			}
-			var_dump($recipients);
 		}
 
+	}
+
+	private function remove_duplicates($arr1) {
+		$arr2 = array();
+		foreach($arr1 as $item) {
+			if (! in_array($item, $arr2) )
+				$arr2[] = $item;
+		}
+		return $arr2;
 	}
 
 
